8bb5a66006ccc8310d5b81e56508ba130029558e
claudio <claudioperezdiaz@gmail.com>
2020-05-04 14:49:26 -0400

    images

--- frontNotos/src/views/index-sections/ListCampaign.js
+++ frontNotos/src/views/index-sections/ListCampaign.js
@@ -2,30 +2,20 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 import 'raf/polyfill';
 import React, { useState, useRef } from 'react';
-import { isEqual } from 'lodash-es';
-import validaRut from 'views/index-sections/rut.js';
-import Modal from 'views/index-sections/modal.js';
-import Index from 'views/Index.js'
+
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import RegisterHeader from "components/Headers/RegisterHeader.js";
 import TransparentFooter from "components/Footers/TransparentFooter.js";
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-import {
-  FormWithConstraints,
-  // Input,
-  FieldFeedbacks,
-  FieldFeedback
-} from 'react-form-with-constraints-bootstrap4';
+import PostForm from './PostForm';
+import Post from './Post';
+import Posts from './Posts';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 
 // reactstrap components
 import {
-  Input,
-  Col,
-  Row,
   Container
 } from "reactstrap";
 
@@ -43,7 +33,40 @@ function ListCampaign() {
       <div className="wrapper">
         <div className="section section-about-us">
           <Container>
-            
+            <div>
+              {/* <Hero header="React Blog" /> */}
+              <BrowserRouter>
+                <div>
+                  {/* <NavBar /> */}
+                  <Container>
+                    <Switch>
+                      <Redirect
+                        from="/home"
+                        to="/create"
+                      />
+                      <Route
+                        path="/"
+                        exact
+                        component={Posts}
+                      />
+                      <Route
+                        path="/post/:_id"
+                        exact
+                        strict
+                        component={Post}
+                      />
+                      <Route
+                        path="/create"
+                        exact
+                        component={PostForm}
+                      />
+                      <Redirect to="/" />
+
+                    </Switch>
+                  </Container>
+                </div>
+              </BrowserRouter>
+            </div>
           </Container>
         </div>
       </div>
